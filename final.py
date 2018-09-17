08e50bbfcd5cf6598097e936ac0a964cad0259c3
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-09-17 16:12:56 -0300

    Otimização

--- final.py
+++ final.py
@@ -3,25 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
+import scipy.signal as sg
+
 import pywt
 
-#file_name = './fase_1/base_treinamento_I/bhhn.wav'
-#file_name = './fase_1/base_treinamento_I/xnmd.wav'
-#file_name = './fase_1/base_treinamento_I/7bxa.wav'
-#file_name = './fase_1/base_treinamento_I/6nxx.wav'
-#file_name = './fase_1/base_treinamento_I/6n77.wav'
+path = "./fase_2/base_treinamento_II/"
+path_test = "./fase_2/base_validacao_II/"
 
-path = "./fase_1/base_treinamento_I/"
-path_test = "./fase_1/base_validacao_I/"
+#path = "./fase_1/base_treinamento_I/"
+#path_test = "./fase_1/base_validacao_I/"
 
-def merge_intervals(intervals,thresh,mult_len,interval_thresh = 2000):
+
+def merge_intervals(intervals,thresh,mult_len,interval_thresh = 2000, interval_min = 500):
+    #plt.vlines(intervals, 0, 2, color='green', linestyle='--',linewidth=3, alpha=0.5, label='Segment boundaries')
     result = []
     i = 1
     result.append(intervals[0])
     while(i<len(intervals)):
         #Verifica distancia entre as duas segmentacoes e se estao dentro dos 2 seg de cada letra
         calc = int((len(result)*mult_len))+1
-        if(intervals[i-1][1] - intervals[i-1][0] < interval_thresh):
+        interval_distance = intervals[i-1][1] - intervals[i-1][0]
+        if(interval_distance < interval_thresh and interval_distance > interval_min): # Itervalo tem que der uma distancia minima e um tam minimo
             result.pop()
             result.append(intervals[i])
         elif(intervals[i][0] - intervals[i-1][1] < thresh and (int(calc/intervals[i-1][1])) == (int(calc/intervals[i][1]))):
@@ -58,7 +60,12 @@ def segment(file_name):
 
     #https://librosa.github.io/librosa/generated/librosa.effects.split.html
 
+
+    y = lowPass(y)
     y = normalize(y)
+    y = filterBySample(y, [1,200],cut_by_max=True)
+
+    
     #y = wavelet(y)
     #y = waveletn(y,'coif1')# haar
     #y = librosa.util.normalize(y)
@@ -73,6 +80,8 @@ def segment(file_name):
     
     #plt.figure(figsize=(12, 4))
     #plt.plot(y)
+    #plt.show()
+
     #plt.vlines(voiced_intervals, 0, 2, color='red', linestyle='--',linewidth=3, alpha=0.5, label='Segment boundaries')
 
 
@@ -86,22 +95,27 @@ def segment(file_name):
     for sg in merged:
         seg.append(y[sg[0]:sg[1]])
 
+    #'''
     for sg in seg:
     
-        #sub_intervals = librosa.effects.split(sg, top_db=25, frame_length=1024, hop_length=100) #Tirando ruído remanescente
-        sub_intervals = librosa.effects.split(sg, top_db=20, frame_length=1024, hop_length=100) #Tirando ruído remanescente
+        sub_intervals = librosa.effects.split(sg, top_db=18, frame_length=1024, hop_length=100) #Tirando ruido remanescente
+        
+        #sub_intervals = librosa.effects.split(sg, top_db=20, frame_length=1024, hop_length=100) #USADO NA OUTRA ENTREGA
         sub_intervals = merge_intervals(sub_intervals,len(sg),1)
     
         interval = sub_intervals.pop()
         filtered = sg[interval[0]:interval[1]]
         seg_filtered.append(filtered)
 
+        #seg_filtered.append(sg)
+
         #plt.figure(figsize=(12, 4))
         #plt.plot(sg)
-        #plt.vlines(interval, 0, 2, color='red', linestyle='--',linewidth=3, alpha=0.5, label='Segment boundaries')
+        #plt.vlines(interval, 0, 0.1, color='red', linestyle='--',linewidth=3, alpha=0.5, label='Segment boundaries')
     
     #plt.show()
-
+    #'''
+    #seg_filtered = seg
     return seg_filtered, sr
 
 
@@ -110,24 +124,54 @@ def extract_features(segments,sr, n_mfcc = 20):
     for segment in segments:
         #Calculandp mfcc
         mfcc = np.mean(librosa.feature.mfcc(y=segment, sr=sr, n_mfcc=n_mfcc).T,axis=0)
+        #tonnetz = librosa.feature.tonnetz(y=segment, sr=sr)
 
+        #print(tonnetz)
         feat = mfcc
 
-        segment = np.array(segment)
+        #segment = np.array(segment)
         
         #Espelhando onda no eixo X
-        segment = abs(segment)
+        #segment = abs(segment)
 
         #Contando quantidade de zeros
-        zeros = len(segment[segment == 0])
-        #features.append(zeros)
-        feat = np.append(feat,zeros)
+        #zeros = len(segment[segment == 0])
+        #feat = np.append(feat,zeros)
+
+        stz = stZCR(segment)
+        ste = stEnergy(segment)
+        stee = stEnergyEntropy(segment)
+        feat = np.append(feat,stz)
+        feat = np.append(feat,ste)
+        feat = np.append(feat,stee)
+
+        # Fourier
+        segment = segment.T
+        stft = np.abs(librosa.stft(segment))
+
+        
+        # chroma
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sr).T,axis=0)
+
+        # melspectrogram
+        mel = np.mean(librosa.feature.melspectrogram(segment, sr=sr).T,axis=0)
+
+        # spectral contrast
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sr).T,axis=0)
+
+        #tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(segment), sr=sr).T,axis=0)
+
+        feat = np.append(feat,chroma)
+        feat = np.append(feat,mel)
+        feat = np.append(feat,contrast)
+        #feat = np.append(feat,tonnetz)
+        
 
         #Calculando itens acima da media
-        mean = segment.mean()
-        over_mean = len(segment[segment >= mean])
-        #features.append(over_mean)
-        feat = np.append(feat,over_mean)
+        #mean = segment.mean()
+        #over_mean = len(segment[segment >= mean])
+        
+        #feat = np.append(feat,over_mean)
 
         #Tamanho
         #total_len = len(segment[segment != 0])
@@ -137,6 +181,56 @@ def extract_features(segments,sr, n_mfcc = 20):
 
     return features
 
+
+def filterBySample(signal, sample, cut_by_max=False, percent_mean=0.7, max_noise = 0.15, default_noise=0.1):
+    #print(len(signal))
+    signal_sample = signal[sample[0]:sample[1]]
+    signal_sample = signal_sample[signal_sample > 0]
+    signal_sample = abs(signal_sample)
+    noise_mean = signal_sample.mean()
+    if(not(cut_by_max)):
+        signal_sample = signal_sample[signal_sample > (noise_mean + noise_mean*percent_mean)]
+        noise_mean = signal_sample.mean()
+    else:
+        noise_mean = signal_sample.max()
+
+    #plt.figure(figsize=(12, 4))
+    #plt.plot(signal)
+    
+
+    if (noise_mean > max_noise):
+        noise_mean = default_noise
+    
+    #noise_mean = 1
+
+    for x in np.nditer(signal, op_flags=['readwrite']) :
+        if(x < 0):
+            if(noise_mean < abs(x)):
+                x += noise_mean
+            else:
+                x *= 0
+            #print(x)
+        else:
+            if(x > noise_mean):
+                x -= noise_mean
+            else:
+                x *= 0
+    #plt.figure(figsize=(12, 4))
+    #plt.plot(signal)
+    
+
+    return signal
+    
+def lowPass(signal):
+    # First, design the Buterworth filter
+    N  = 5    # 3, 4 Filter order
+    Wn = 0.08 # 0.1 Cutoff frequency
+    B, A = sg.butter(N, Wn, output='ba')
+    smooth_data = sg.filtfilt(B,A, signal)
+    #plt.plot(signal,'r-')
+    #plt.plot(smooth_data,'b-')
+    #plt.show()
+    return smooth_data
 
 def normalize(signal,pre_emphasis= 0.97):
     emphasized_signal = np.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
@@ -157,6 +251,39 @@ def getLabels(path_file):
 
 def main():
     getFiles(path)
+
+# ===== Retirei de uma lib/paper : https://github.com/tyiannak/pyAudioAnalysis
+
+eps = 0.00000001
+
+def stZCR(frame):
+    """Computes zero crossing rate of frame"""
+    count = len(frame)
+    countZ = np.sum(np.abs(np.diff(np.sign(frame)))) / 2
+    return (np.float64(countZ) / np.float64(count-1.0))
+
+
+def stEnergy(frame):
+    """Computes signal energy of frame"""
+    return np.sum(frame ** 2) / np.float64(len(frame))
+
+
+def stEnergyEntropy(frame, n_short_blocks=10):
+    """Computes entropy of energy"""
+    Eol = np.sum(frame ** 2)    # total frame energy
+    L = len(frame)
+    sub_win_len = int(np.floor(L / n_short_blocks))
+    if L != sub_win_len * n_short_blocks:
+            frame = frame[0:sub_win_len * n_short_blocks]
+    # sub_wins is of size [n_short_blocks x L]
+    sub_wins = frame.reshape(sub_win_len, n_short_blocks, order='F').copy()
+
+    # Compute normalized sub-frame energies:
+    s = np.sum(sub_wins ** 2, axis=0) / (Eol + eps)
+
+    # Compute entropy of the normalized sub-frame energies:
+    Entropy = -np.sum(s * np.log2(s + eps))
+    return Entropy
 
 
 #main()
--- raciocinio.py
+++ raciocinio.py
@@ -20,7 +20,6 @@ from sklearn import model_selection
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 
 import random
@@ -112,8 +111,7 @@ models_name= [
     "Naive Bayes",
     "SVM",
     "Gaussian Process",
-    "Neural Net",
-    "Bagging"
+    "Neural Net"
 ]
 
 models = [
@@ -127,17 +125,16 @@ models = [
     GridSearchCV(SVC(kernel='rbf', class_weight='balanced',probability=True), param_grid), #PCA(svd_solver='randomized',whiten=True),
     GaussianProcessClassifier(1.0 * RBF(1.0)),
     MLPClassifier(random_state=1,hidden_layer_sizes=(32,16),activation='relu'),
-    BaggingClassifier(n_jobs=3, random_state=0)
     ]
 
 
 # Selecionando modelos e setando pesos relativos
 print("Selecionando modelos")
 
-selected_models = [0,1,4,5,6]
+selected_models = [0,1,5,6,4]
 #selected_models = [0,1,2,3,4,5,6]
 #selected_models = [0,1,4,5]
-relative_wheigths = [2,2,1,4,3,2]
+relative_wheigths = [2,2,4,3,1]
 
 aux1 = []
 aux2 = []
@@ -154,13 +151,62 @@ models = aux2
 
 print("\n#ETAPA 3 - Treinamento")
 
+'''
+lenData = len(y)
+X1 = X[int(lenData*0.3):]
+y1 = y[int(lenData*0.3):]
+
+X2 = X[:int(lenData*0.3)]
+y2 = y[:int(lenData*0.3)]
+'''
+
+'''
+i = 0
+print("Treinando modelos")
+for model in models:
+    print("\nTreinando: "+models_name[i])
+    model.fit(X,y)
+    i+=1
+'''
+
 # ENSEMBLE
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 print("Treinando pelo Ensemble")
-#eclf1 = VotingClassifier(estimators=estimators, weights=relative_wheigths, voting='soft', n_jobs=3)
-eclf1 = VotingClassifier(estimators=estimators, voting='hard', n_jobs=1)
+eclf1 = VotingClassifier(estimators=estimators, voting='soft', n_jobs=3)
 eclf1 = eclf1.fit(X, y)
 
+'''
+# Stack de metodos
+mStack = GaussianNB()
+#mStack = RandomForestClassifier(n_jobs=4, random_state=1)
+#mStack = MLPClassifier(random_state=1,hidden_layer_sizes=(32,16),activation='relu')
+#mStack = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+
+print("Treinando stack")
+i = 0
+#yy = y[:29]
+Xs = []
+for yn in y:
+    p_temp = []
+    #debug
+    if(i%10 == 0): print("Retreinados:",i,"/",len(y))
+    j = 0
+    for model in models:
+        #print("\nPredizendo: "+models_name[i])
+        p_temp.append(mc_labels.index(model.predict_proba([X[i]])))
+        #print(j)
+        #for w in range(relative_wheigths[j]): # Peso relativo
+        #        p_temp.append(mc_labels.index(model.predict([X[i]])[0])+1)
+        j+=1
+
+    #print(np.array([p_temp]),[yn])
+    #mStack.fit(np.array([p_temp]),[yn])
+    Xs.append(p_temp)
+    i+=1
+
+mStack.fit(np.array(Xs),y)
+'''
 
 # Predicao do teste
 #'''
@@ -171,18 +217,71 @@ j = 0
 i = 0
 captcha_solveds = 0
 success = 0
+statistics = [[],[]] # [pred, labels]
+
+
+p_model = 2
 
 for ytn in yt:
     i = 0
+    predicteds = [[],[],[],[]]
     result = []
  
+    p_temp = [[],[],[],[]]
+    p_temp_code = [[],[],[],[]]
      #debug
     if(j%10 == 0): print("Preditos:",j,"/",len(yt))
+    '''
+    for model in models:
+        #print("\nPredizendo: "+models_name[i])
+        p = model.predict_proba(Xt[j])
+        for k in range(4):
+            p_temp[k].append(p[k])
+            p_temp_code[k].append(mc_labels.index(p[k])+1)
+
+            #for w in range(relative_wheigths[i]): # Peso relativo
+            #    predicteds[k].append(mc_labels.index(p[k])+1)
+        i+=1
+    '''
     
-    #Predizendo letra a letra
+    #print(predicteds)
+    #Escolhendo
     for k in range(4):
+        '''
+        # Stack
+        #yyy= mStack.predict(np.array([p_temp_code[k]]))
+        #print(p_temp_code[k],yyy)
+        #result.append(yyy[0])
+        '''
         yyy = eclf1.predict([Xt[j][k]])[0]
+        #print(yyy)
         result.append(yyy)
+
+        '''
+        # Euristica mista
+        if(p_temp[k][p_model] in ['n','b']): # Se for algum que o SVM entra em colizao# ->n,b,6
+            if(p_temp[k][p_model] == 'n'): # SVM tem mta colizao com m (diz que e 'm') - se for desempata com outros modelos
+                if(p_temp[k][4] == 'h'): # Se NB Disse que e h -> prob de ser N aumenta
+                    for w in range(relative_wheigths[4]):
+                        predicteds[k].append('m')
+                if(p_temp[k][3] == 'n'): # Se Gaussian Disse que e n -> prob de ser M aumenta
+                    for w in range(relative_wheigths[3]):
+                        predicteds[k].append('m')
+            elif(p_temp[k][p_model] == 'b'): # fala que e b mas a maioria das vezes e d
+                #Gaussian confunde pouco B com D
+                if(p_temp[k][3] == 'b'): # Confio no Gaussian se ele disser que e 'b'
+                    for w in range(relative_wheigths[3]):
+                        predicteds[k].append('b')
+            aux = predicteds[k]#[relative_wheigths[0]:]
+            ct = Counter(aux)
+            ct = ct.most_common()[0][0]
+            result.append(ct)
+        else: # se nao, SVM manda
+            result.append(p_temp[k][p_model])   
+        #ct = Counter(predicteds[k])
+        #ct = ct.most_common()[0][0]
+        #result.append(ct)
+        '''
     result = np.array(result)
     #print(result, ytn)
     c = ytn[ytn == result]
@@ -195,23 +294,19 @@ for ytn in yt:
 print("\n#ETAPA 5 - RESULTADOS: ")
 print("Caracteres corretos: ",success,"de",len(yt)*4)
 print("Captchas corretos: ",captcha_solveds,"de",len(yt))
-
-
 #'''
+
+
 
 # Código utilizado para selecao dos modelos
 # ==== ANALISANDO MODELOS
 '''
-i = 0
-print("Treinando modelos")
-for model in models:
-    print("\nTreinando: "+models_name[i])
-    model.fit(X,y)
-    i+=1
-
+c_models = []
+c_models.append(models)
+c_models.append(mStack)
 j = 0
 print("Predizendo")
-for model in models:
+for model in c_models:
     i = 0
     captcha_solveds = 0
     success = 0
@@ -233,4 +328,44 @@ for model in models:
     print(mc_labels)
     print(confusion_matrix(statistics[1], statistics[0],labels = mc_labels))
     j+=1
+'''
+
+# == BAGGING
+'''
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+
+seed = 7
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+# create the sub models
+#estimators = []
+#model1 = LogisticRegression()
+#estimators.append(('logistic', model1))
+#model2 = DecisionTreeClassifier()
+#estimators.append(('cart', model2))
+#model3 = SVC()
+#estimators.append(('svm', model3))
+# create the ensemble model
+
+
+ensemble = VotingClassifier(models)
+results = model_selection.cross_val_score(ensemble, X, y, cv=kfold)
+print(results.mean())
+
+
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+cart = DecisionTreeClassifier()
+num_trees = 100
+model = BaggingClassifier(base_estimator=cart, n_estimators=num_trees, random_state=seed)
+results = model_selection.cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
+
+#print("\n\nTREINO ",y)
+#print("\nTESTE: ",yt)
+
 '''
